bd644c1444bf13988d1ab683c37431181c35e438
VARUN SHARMA <vsofficialmail@gmail.com>
2021-05-27 13:48:09 +0530

    Adding app view page

--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,8 @@ from .views import app_form, AppView
 from django.urls import reverse_lazy
 from .models import AppModel
 from apps import admin
+from django.conf import settings
+from django.conf.urls.static import static
 app_name='apps'
 
 
@@ -14,3 +16,5 @@ urlpatterns = [
     path("b/<slug>", AppView.as_view(), name="appview"),
    # path("<str:appname>", AppDetailView.as_view(), name="appname" ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

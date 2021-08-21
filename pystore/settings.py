a8a160c34d9839e1c6f1bcf86082367c8d85db91
VARUN SHARMA <vsofficialmail@gmail.com>
2021-08-22 02:03:16 +0530

    Updating Settings
    Updating Settings to include 'pythonstore.herokuapp.com' in ALLOWED_HOST

--- pystore/settings.py
+++ pystore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'pxz!5e7vki$4%fx2+xb-z6v_85b)0j)nwontigspp#!+fpxu*2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythonstore.herokuapp.com']
 
 
 # Application definition

6dba03999055f6609351b6f6e86ca88cda44425c
Varun Sharma <vsofficialmail@gmail.com>
2022-08-26 02:01:20 +0530

    repair

--- apps/urls.py
+++ apps/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("upload_formset", app_form, name="appformset"),
     path("apps/<slug>", AppView.as_view(), name="appview"),
     path("home/", AppListView.as_view(), name="applist"),
-    path("search/<str:inputValue>", SearchList, name="search"),
+    path("search/", SearchList, name="search"),
    # path("<str:appname>", AppDetailView.as_view(), name="appname" ),
 ]
 
